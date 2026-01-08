7a9cf4956f45dd7b00d86c7415187fb3f5b412d6
billcat_codemag <billcat.codemag@distens.com>
2026-01-08 10:43:28 -0500

    init

--- hw.py
+++
@@ -1,5 +0,0 @@
-print("Waiting for debugger attach...") # When run in debug mode, this line will block until the debugger connects.
-print("Debugger attached, continuing execution.")
-
-my_name = "Bill"
-print("Hello, " + my_name)
